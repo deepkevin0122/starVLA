c53d7da612cbcff5992a367bf2210aaa7fd90329
Deep_kevin <v_kevin.zhao@smartmore.com>
2026-01-13 17:47:45 +0800

    1.13.4

--- start.sh
+++ start.sh
@@ -7,11 +7,6 @@ trap "kill 0" SIGINT SIGTERM EXIT
 (
   conda activate starVLA
   bash examples/LIBERO/eval_files/run_policy_server.sh
-) &
-
-(
-  conda activate libero
-  bash examples/LIBERO/eval_files/eval_libero.sh
-)
+) 
 
 wait
