f9734084048ec1a06587227017037d4b51bb79f7
Deep_kevin <v_kevin.zhao@smartmore.com>
2026-01-19 09:42:52 +0800

    check

--- examples/LIBERO/train_files/run_libero_train.sh
+++ examples/LIBERO/train_files/run_libero_train.sh
@@ -10,7 +10,7 @@ export NCCL_TIMEOUT=10000  # timeout set to 1 hour (unit: seconds)
 export NCCL_SOCKET_TIMEOUT_MS=360000
 ###########################################################################################
 # === Please modify the following paths according to your environment ===
-Framework_name=QwenPI
+Framework_name=QwenGR00T
 freeze_module_list=''
 base_vlm=playground/Pretrained_models/Qwen3-VL-4B-Instruct
 config_yaml=./examples/LIBERO/train_files/starvla_cotrain_libero.yaml
